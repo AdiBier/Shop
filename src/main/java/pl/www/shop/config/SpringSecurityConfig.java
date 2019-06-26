4466edaaef96c20b9e69fd0190ee5a86aad22e78
AdiBier <AdiBier@interia.eu>
2019-06-26 17:40:12 +0100

    good ver

---
+++ src/main/java/pl/www/shop/config/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package pl.www.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/static/**").permitAll()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
